17977b06b6e34a979485930dfd844ee184c86e91
Rakib Linux <rakibulinux@gmail.com>
2022-10-25 08:42:25 +0600

    categories added

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require("./data/categories.json");
 const courses = require("./data/courses.json");
 
 app.get("/", (req, res) => {
   res.send("API Server is runing");
 });
 
+app.get("/categories", (req, res) => {
+  res.send(courses);
+});
 app.get("/courses", (req, res) => {
   res.send(courses);
 });
